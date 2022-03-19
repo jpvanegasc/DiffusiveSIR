bf979d46b6861635ac9d93981928ecd093080aff
Juan Pablo Vanegas <juan.vanegas@loka.com>
2022-03-19 15:00:49 -0500

    :rotating_light: Linter changes

--- src/plot.py
+++ src/plot.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 
 
 def plot_circle(x: float, y: float):
-    plt.scatter(x, y, color='black')
+    plt.scatter(x, y, color="black")
 
 
 def plot_particles(particles: np.array, xlim=1, ylim=1):
     for p in particles:
         plot_circle(p[0], p[1])
 
-    plt.xlim([0,xlim])
-    plt.ylim([0,ylim])
+    plt.xlim([0, xlim])
+    plt.ylim([0, ylim])
