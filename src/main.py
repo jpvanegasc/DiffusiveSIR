3275b94faf5d8d6c656c44769d0e8a6ed1400636
Juan Pablo Vanegas <juan.vanegas@loka.com>
2022-04-05 12:18:20 -0500

    :recycle: Parametrize N & t_max

--- src/main.py
+++ src/main.py
@@ -1,30 +1,33 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 from diffusive_sir import DiffusiveSIR
 from plot import plot_particles
 
 
-def main():
-    d = DiffusiveSIR(100, 0.01, 0.012)
+def main(N, t_max):
+    d = DiffusiveSIR(N, 0.01, 0.012)
 
-    colors = list(map(lambda h: d.get_health_color(h), d.health))
+    colors = list(map(lambda h: d.get_health_color(h), d.particles[:, 2]))
     plot_particles(d.particles, d.L, d.L, color=colors)
     plt.savefig("../data/initial.png")
     plt.close()
 
-    d.evolve(9000)
+    d.evolve(t_max)
 
-    colors = list(map(lambda h: d.get_health_color(h), d.health))
+    colors = list(map(lambda h: d.get_health_color(h), d.particles[:, 2]))
     plot_particles(d.particles, d.L, d.L, color=colors)
     plt.savefig("../data/final.png")
     plt.close()
 
-    plt.plot(d.healthy[:, 0], d.healthy[:, 1], color="green")
-    plt.plot(d.infected[:, 0], d.infected[:, 1], color="red")
-    plt.plot(d.recovered[:, 0], d.recovered[:, 1], color="yellow")
+    t = np.arange(t_max)
+
+    plt.plot(t, d.sir[:, 0], color="green")
+    plt.plot(t, d.sir[:, 1], color="red")
+    plt.plot(t, d.sir[:, 2], color="yellow")
     plt.savefig("../data/sir.png")
     plt.close()
 
 
 if __name__ == "__main__":
-    main()
+    main(1000, 1000)
