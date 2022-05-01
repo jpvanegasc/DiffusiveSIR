5416c8fb94b387be91f683622eec9341307f33c8
jpradas1 <jpradas@unal.edu.co>
2022-04-30 23:04:07 -0500

    finally, I solved the problem of evolving in ergodicity.py

--- src/ergodicity.py
+++ src/ergodicity.py
@@ -10,7 +10,6 @@ d = DiffusiveSIR(N, 0.01, 0.012)
 
 
 def time_average_MSD(d, Nstep):
-    d.particles[0,:2] = d.L * np.random.rand(1, 2)
     X = d.particles[0,:2]
     dx, dy = 0, 0
     xn = X[0]
@@ -39,9 +38,10 @@ def time_average_MSD(d, Nstep):
     return tMSD , RX , RY
 
 def ensemble_average_MSD(d, Nstep):
-    #d.particles[:, :2] = d.L * np.random.rand(d.N, 2)
     X0 = d.particles[:, :2]
-    X = d.particles[:, :2]
+    X0 = np.array(X0)
+    X = d.particles[:, :2],
+    X = np.array(X)
     mu, sigma = 0.0, 2.0 * d.D * d.dt
     const = np.sqrt(sigma)
     Daniel = 2 * np.sqrt(np.pi)
@@ -49,30 +49,33 @@ def ensemble_average_MSD(d, Nstep):
 
     for t in range(Nstep):
         dx = const * np.random.normal(size=(d.N, 2))*(1/Daniel)
+        dx = np.array(dx)
         X[:,:] += dx
 
-        for kk in range(d.N):
-            if(X[kk][0] >= d.L): X[kk][0] -= 2 * dx[kk][0]
-            if(X[kk][1] >= d.L): X[kk][1] -= 2 * dx[kk][1]
+        if(X[:, 0] >= d.L).all(): X[kk][0] -= 2 * dx[kk][0]
+        if(X[:, 1] >= d.L).all(): X[kk][1] -= 2 * dx[kk][1]
 
-    XN = X
-    #x , y = XN[0] , XN[1]
+    XN = X[0]
 
-    # for ii in range(d.N):
-    #     msd += ( XN[ii, :1] - X0[ii, :1] )**2 + ( XN[ii, 1:2] - X0[ii, 1:2] )**2
-    #
-    # eMSD = msd / d.N
-    return X0, XN
+    for ii in range(d.N):
+        msd += ( XN[ii, 0] - X0[ii, 0] )**2 + ( XN[ii, 1] - X0[ii, 1] )**2
 
-R0, RN = ensemble_average_MSD(d, 100)
-print("R0 = ", R0)
-print("RN = ", RN)
-# print("y = ", y)
+    eMSD = msd / (d.N * Nstep)
+    return eMSD , X0, XN
+
+# R0, RN = ensemble_average_MSD(d, 100)
+# print("R0 = ", R0)
+# print("RN = ", RN)
 
 tMSD, RX, RY = time_average_MSD(d, 1000)
-# eMSD = ensemble_average_MSD(d, 1000)
+eMSD, R0, RN = ensemble_average_MSD(d, 1000)
 print(tMSD)
-# print(eMSD)
+print(eMSD)
+err = abs(tMSD - eMSD)/(tMSD)
+print("error = ", err)
+
+plt.scatter(R0[:,0], R0[:,1],  color = "red" , label = 'Initial' )
+plt.scatter(RN[:,0], RN[:,1], color = "green" , label = 'Final' )
 
 plt.plot(RX, RY, color='cyan', label='Camino aleatorio')
 plt.legend()
